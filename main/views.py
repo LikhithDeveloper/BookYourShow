6b947bf60c20208742a3221ab53b648dcad9110f
LikhithDeveloper <149604389+LikhithDeveloper@users.noreply.github.com>
2024-06-02 07:19:13 +0530

    Update views.py

--- main/views.py
+++ main/views.py
@@ -9,7 +9,6 @@ from django.contrib.auth.decorators import login_required
 
 
 
-# Create your views here.
 @login_required(login_url='/login/')
 def book(request):
     queryset = Book.objects.all()
